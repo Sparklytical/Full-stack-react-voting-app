38316b12fca8b186cbc55bad0a8ba8fe3ee74dbe
EvilSpark <kamrantahir25@gmail.com>
2019-02-16 01:53:50 +0530

    last finished commit

---
+++ public/js/app.js
@@ -0,0 +1,12 @@
+class ProductList extends React.component {
+    render () {
+        return (
+            <div className=' ui unstackable items'>
+                Hello, Friend! I am a basic React component
+            </div>
+        );
+    }
+}
+var setDefaultBrowser = require('set-default-browser');
+
+setDefaultBrowser("firefox");
